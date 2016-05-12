ae318d43bf7596f35b488a3914a168ce6d808934
darkalucard2303 <erick_leonel_94@hotmail.com>
2016-05-12 11:41:38 -0600

    Update Prueba.java

--- Prueba/src/prueba/Prueba.java
+++ Prueba/src/prueba/Prueba.java
@@ -17,7 +17,7 @@ public class Prueba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        JOptionPane.showConfirmDialog(null, "Ptueba");
+        JOptionPane.showConfirmDialog(null, "Prueba");
     }
     
 }
